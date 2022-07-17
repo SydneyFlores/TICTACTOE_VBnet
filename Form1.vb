e0c63614b60287f0dda5aa31875a6245ab1c6ba6
SydneyFlores <mharsydney@gmail.com>
2022-07-17 21:46:29 +0800

    button working fine

--- Form1.vb
+++ Form1.vb
@@ -16,7 +16,33 @@
             turn = True
 
         End If
+        tictactoe.Enabled = False
+
 
 
     End Sub
+    Public Sub Checker()
+        '    
+        If btn1.Text.Equals(btn2.Text) And btn2.Text.Equals(btn3.Text) And btn1.Text <> "" Then
+
+
+        ElseIf btn4.Text.Equals(btn5.Text) And btn5.Text.Equals(btn6.Text) And btn4.Text <> "" Then
+
+
+        ElseIf btn7.Text.Equals(btn8.Text) And btn8.Text.Equals(btn9.Text) And btn7.Text <> "" Then
+
+        ElseIf btn1.Text.Equals(btn4.Text) And btn4.Text.Equals(btn7.Text) And btn1.Text <> "" Then
+
+        ElseIf btn2.Text = btn5.Text And btn5.Text = btn8.Text And btn2.Text <> "" Then
+
+        ElseIf btn3.Text = btn6.Text And btn6.Text = btn9.Text And btn3.Text <> "" Then
+
+        ElseIf btn1.Text.Equals(btn5.Text) And btn5.Text.Equals(btn9.Text) And btn1.Text <> "" Then
+
+        ElseIf btn3.Text.Equals(btn5.Text) And btn5.Text.Equals(btn7.Text) And btn3.Text <> "" Then
+
+        End If
+
+    End Sub
+
 End Class
