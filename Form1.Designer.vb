cd4cd948b306c6c51439714121516666190d5482
SydneyFlores <mharsydney@gmail.com>
2022-07-17 21:53:47 +0800

    Winner recognize

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -32,6 +32,8 @@ Partial Class Form1
         Me.btn8 = New System.Windows.Forms.Button()
         Me.btn9 = New System.Windows.Forms.Button()
         Me.btnReset = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btn1
@@ -125,12 +127,34 @@ Partial Class Form1
         Me.btnReset.Text = "Reset"
         Me.btnReset.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(55, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 25)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Player X"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(55, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 25)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Player O"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.ClientSize = New System.Drawing.Size(572, 655)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnReset)
         Me.Controls.Add(Me.btn9)
         Me.Controls.Add(Me.btn8)
@@ -144,6 +168,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -157,4 +182,6 @@ Partial Class Form1
     Friend WithEvents btn8 As Button
     Friend WithEvents btn9 As Button
     Friend WithEvents btnReset As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -7,7 +7,7 @@
 
         If turn Then
             tictactoe.Text = "X"
-            'inverting the value 
+
             turn = False
 
         Else
@@ -17,32 +17,83 @@
 
         End If
         tictactoe.Enabled = False
-
+        Checker()
 
 
     End Sub
     Public Sub Checker()
         '    
         If btn1.Text.Equals(btn2.Text) And btn2.Text.Equals(btn3.Text) And btn1.Text <> "" Then
+            If btn1.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
 
         ElseIf btn4.Text.Equals(btn5.Text) And btn5.Text.Equals(btn6.Text) And btn4.Text <> "" Then
+            If btn4.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
 
         ElseIf btn7.Text.Equals(btn8.Text) And btn8.Text.Equals(btn9.Text) And btn7.Text <> "" Then
+            If btn7.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
         ElseIf btn1.Text.Equals(btn4.Text) And btn4.Text.Equals(btn7.Text) And btn1.Text <> "" Then
+            If btn1.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
         ElseIf btn2.Text = btn5.Text And btn5.Text = btn8.Text And btn2.Text <> "" Then
+            If btn2.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
         ElseIf btn3.Text = btn6.Text And btn6.Text = btn9.Text And btn3.Text <> "" Then
+            If btn3.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
         ElseIf btn1.Text.Equals(btn5.Text) And btn5.Text.Equals(btn9.Text) And btn1.Text <> "" Then
+            If btn1.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
         ElseIf btn3.Text.Equals(btn5.Text) And btn5.Text.Equals(btn7.Text) And btn3.Text <> "" Then
+            If btn3.Text.Equals("X") Then
+                MessageBox.Show("Player X wins")
+            Else
+                MessageBox.Show("Player O wins")
 
+
+            End If
         End If
 
     End Sub
 
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
