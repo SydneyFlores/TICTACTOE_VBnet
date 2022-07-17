4c00945cfcac46f15d16424b50ac4c24310262f4
SydneyFlores <mharsydney@gmail.com>
2022-07-17 21:13:19 +0800

    buttons added

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,139 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn1.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(55, 130)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(150, 137)
+        Me.btn1.TabIndex = 0
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(211, 130)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(150, 137)
+        Me.btn2.TabIndex = 1
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(367, 130)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(150, 137)
+        Me.btn3.TabIndex = 2
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(55, 273)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(150, 137)
+        Me.btn4.TabIndex = 3
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(211, 273)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(150, 137)
+        Me.btn5.TabIndex = 4
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(367, 273)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(150, 137)
+        Me.btn6.TabIndex = 5
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(55, 416)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(150, 137)
+        Me.btn7.TabIndex = 6
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(211, 416)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(150, 137)
+        Me.btn8.TabIndex = 7
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Minion Pro", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(367, 416)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(150, 137)
+        Me.btn9.TabIndex = 8
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(381, 571)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(136, 52)
+        Me.btnReset.TabIndex = 9
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(572, 655)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnReset As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btn4.Click
 
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+
+    End Sub
 End Class
