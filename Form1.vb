124591a86fb320ba47edc5e561a78e8c65ed3bf0
SydneyFlores <mharsydney@gmail.com>
2022-07-17 21:31:13 +0800

    buttons working properly

--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,22 @@
 ﻿Public Class Form1
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+    Dim turn As Boolean = True
 
-    End Sub
+    Private Sub btns(sender As Object, e As EventArgs) Handles btn9.Click, btn8.Click, btn7.Click, btn6.Click, btn5.Click, btn4.Click, btn3.Click, btn2.Click, btn1.Click
 
-    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        Dim tictactoe As Button = CType(sender, Button)
+
+        If turn Then
+            tictactoe.Text = "X"
+            'inverting the value 
+            turn = False
+
+        Else
+            tictactoe.Text = "O"
+
+            turn = True
+
+        End If
+
 
     End Sub
 End Class
